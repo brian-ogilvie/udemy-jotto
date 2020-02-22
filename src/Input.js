7c2bc6b870a2a0b4936baceaabf3aa8f6358810c
Brian Ogilvie <brian@brianogilviedesigns.com>
2020-02-22 07:09:27 -0500

    add blank Input form

--- src/Input.js
+++ src/Input.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-function Input() {
-  return <div></div>;
+function Input({ success }) {
+  const contents = success ? null : (
+    <form className="form-inline">
+      <input
+        data-test="input-box"
+        className="mb-2 mb-sm-3"
+        type="test"
+        placeholder="enter guess"
+      />
+      <button
+        data-test="submit-button"
+        type="submit"
+        className="btn btn-primary mb-2"
+      >
+        Guess
+      </button>
+    </form>
+  );
+  return <div data-test="component-input">{contents}</div>;
 }
 
 const mapStateToProps = state => ({});
