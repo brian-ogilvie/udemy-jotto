2b6417ee879654b6c6df7b09dbf9115d5c1e4129
Brian Ogilvie <brian@brianogilviedesigns.com>
2020-02-21 20:01:37 -0500

    setupTests.js

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
 import Congrats from './Congrats';
 import { findByTestAttr, checkProps } from '../test/testUtils';
-
-Enzyme.configure({ adapter: new Adapter() });
 
 const defaultProps = { success: true };
 
