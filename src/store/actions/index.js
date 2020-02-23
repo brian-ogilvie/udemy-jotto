87b0886282cb4b51bd08dbb5922210652a736496
Brian Ogilvie <brian@brianogilviedesigns.com>
2020-02-22 20:18:35 -0500

    guessWord action creator

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,4 +1,14 @@
 import types from '../actionTypes';
 import { getLetterMatchCount } from '../../helpers/';
 
-export const guessWord = guessedWord => (dispatch, getState) => {};
+export const guessWord = guessedWord => (dispatch, getState) => {
+  const { secretWord } = getState();
+  const letterMatchCount = getLetterMatchCount(guessedWord, secretWord);
+  dispatch({
+    type: types.GUESS_WORD,
+    payload: { guessedWord, letterMatchCount }
+  });
+  if (guessedWord === secretWord) {
+    dispatch({ type: types.CORRECT_GUESS });
+  }
+};
--- src/store/reducers/guessedWordsReducer.js
+++ src/store/reducers/guessedWordsReducer.js
@@ -4,6 +4,8 @@ const initialState = [];
 
 export default function guessedWords(state = initialState, action) {
   switch (action.type) {
+    case types.GUESS_WORD:
+      return [...state, action.payload];
     default:
       return state;
   }
